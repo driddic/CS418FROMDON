561501a001ebb9ec63c366ff402f87b056d529ac
driddic <35910492+driddic@users.noreply.github.com>
2018-10-01 17:00:34 -0400

    Create interests.php

---
+++ interests.php
@@ -0,0 +1,17 @@
+<?php
+
+
+<form action="/action_page.php">
+  <select name="groups">
+    <option value="academic">Academic</option>
+    <option value="sports">Sports</option>
+    <option value="arts">Arts</option>
+    <option value="music">Music</option>
+  </select>
+  <br><br>
+  <input type="submit">
+</form>
+
+
+
+?>
