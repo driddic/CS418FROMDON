18780533e6b3e7784cae42a0568f72ca7ec92388
driddic <driddic@cs.odu.edu>
2018-11-27 15:28:19 -0500

    login bug 2.0 fix

--- login.php
+++ login.php
@@ -22,55 +22,31 @@ if(isset ($_POST['submit']))
             }
 
             else {
-                      //trying this
+                      //trying this but it does not work in docker
                        $sql = "SELECT * FROM users WHERE uname='".$uname."' and pword= '".$pwd."';";
-                      $statement = mysqli_stmt_init($conn);
-                      if(!mysqli_stmt_prepare($statement, $sql))
-                      {
-                        header("location: index.php?badsql");
-                        exit();
-                      }
-                      elseif(mysqli_stmt_prepare($statement, $sql)) {
-                                //if password is wrong I get a error on this line
+                       $result = mysqli_query($conn,$sql);
 
-                                mysqli_stmt_bind_param($statement,"ss", $uname, $pwd);
-                                mysqli_stmt_execute($statement);
-                                $result = mysqli_stmt_get_result($statement);
+                       if ($row = mysqli_fetch_assoc($result))   {
+                                   //if true start a session here
+                                 //session_start();
+                                 $_SESSION['userid'] = $row['userid'];
+                                 $_SESSION['username'] = $row['uname'];
+                                 $_SESSION['password'] = $row['pwd'];
 
-                                if($row = mysqli_fetch_assoc($result)){ // code...
+                                 //$_SESSION['logged_in']= true;
+                                 header("Location: homepage.php");
+                                 exit();
+                       }
 
-                                  // if (!mysqli_stmt_bind_param($statement,"ss", $uname, $pwd)) {
-                                  //   header("location: index.php?badpwd22");
-                                  //   exit();
-                                  // }
-
-                                   $pwdCheck= $row['pword'];
-
-                                      if (!$pwdCheck == $pwd) {
-                                        header("location: index.php?badpwd");
-                                        exit();// code...
-                                      }
-                                      elseif ($pwdCheck==$pwd) {
-                                        //session_start();
-                                        $_SESSION['userid']= $row['userid'];
-                                        $_SESSION['username']= $row['uname'];
-                                        //$_SESSION['password']= $row['pword'];
-
-                                        header("Location: homepage.php");
-                                                   exit();// code...
-                                      }
-                                      else { //incase of a mistake , safe case
-                                              Header("Location: index.php?error=noacct");
-                                              exit();
-
-                                            }
-
-                                  }
-                        }
-
-
-                    }
+                        else { //incase of a mistake , safe case
+                              Header("Location: index.php?error=noacct");
+                              exit();
+                            }
                   }
+}
+
+
+
 
                     // if we can result database against empty fields
                     //  $sql = "SELECT * FROM users WHERE uname='".$uname."' and pword= '".$pwd."';";
@@ -106,3 +82,60 @@ if(isset ($_POST['submit']))
                   }
 
 ?>
+
+
+<!-- $statement = mysqli_stmt_init($conn);
+if(!mysqli_stmt_prepare($statement, $sql))
+{
+  header("location: index.php?badsql");
+  exit();
+}
+elseif(mysqli_stmt_prepare($statement, $sql)) {
+          //if password is wrong I get a error on this line
+
+          mysqli_stmt_bind_param($statement,"ss", $uname, $pwd);
+          mysqli_stmt_execute($statement);
+          $result = mysqli_stmt_get_result($statement);
+
+          if($row = mysqli_fetch_assoc($result)){ // code...
+
+            // if (!mysqli_stmt_bind_param($statement,"ss", $uname, $pwd)) {
+            //   header("location: index.php?badpwd22");
+            //   exit();
+            // }
+
+             $pwdCheck= $row['pword'];
+
+                if (!$pwdCheck == $pwd) {
+                  header("location: index.php?badpwd");
+                  exit();// code...
+                }
+                elseif ($pwdCheck==$pwd) {
+                  //session_start();
+                  $_SESSION['userid']= $row['userid'];
+                  $_SESSION['username']= $row['uname'];
+                  //$_SESSION['password']= $row['pword'];
+
+                  header("Location: homepage.php");
+                             exit();// code...
+                } -->
+
+<!--
+// if ($row = mysqli_fetch_assoc($result))
+//
+//
+//
+// {
+//             //if true start a session here
+//           session_start();
+//           $_SESSION['userid'] = $row['userid'];
+//           $_SESSION['uname'] = $row['uname'];
+//           $_SESSION['password'] = $row['pwd'];
+//
+//           //$_SESSION['logged_in']= true;
+//           header("Location: homepage.php");
+//           exit();
+//
+//
+//
+// } -->
