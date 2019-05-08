c845eca2a79d645a3270dcaeacd40f32a1da013a
driddic <driddic@cs.odu.edu>
2019-05-08 17:18:17 -0400

    successfull image posting. finally.

--- homepage.php
+++ homepage.php
@@ -11,14 +11,18 @@
   <head>
     <meta charset="utf-8">
     <title>homepage</title>
+
+    <!--
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    -->
     <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.2.2/jquery.form.js"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
       <!--
     -->
      <style media="screen">
@@ -126,6 +130,10 @@
              -ms-transform: translateX(26px);
              transform: translateX(26px);
            }
+           .box {
+           width:600px;
+           margin:0 auto;
+          }
      </style>
   </head>
   <body>
@@ -229,7 +237,7 @@ else {
   <div class='container'>
   <form id='comment_form' class="w3-form-post" enctype="multipart/form-data" >
    <div class="form-group">
-     <textarea name='comment_content' id='comment_content' class='form-control' placeholder='Enter Comment' rows='5'>
+     <textarea name='comment_content' id='comment_content' class='form-control' placeholder='Enter Comment' rows='5' required>
      </textarea>
    </div>
     <div class="form-group">
@@ -244,36 +252,168 @@ else {
      <input type='submit' name='submit' id='submit'   value='Post'>
      </div>
    </form>
-   <span id='comment_message'></span>
+   <!-- Trigger/Open The Modal -->
+    <button id="myBtn">Post Pictures Using URL</button>
+    <!-- The Modal -->
+    <div id="myModal" class="modal">
+     <!-- Modal content -->
+     <div class="modal-content">
+       <span class="close">&times;</span>
+       <div class="container box">
 
+       <div class="form-group">
+         <label>Enter Image Url</label>
+         <input type="text" name="image_url" id="image_url" class="form-control" />
+       </div>
+       <div class="form-group">
+         <input type="button" name="upload" id="upload" value="Upload" class="btn btn-info" />
+       </div>
+   <br />
+   <div id="result"><img src = "upload/" class="img-thumbnail img-responsive" /></div>
+  </div>
+</div>
+  <div style="clear:both"></div>
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" />
+  <script type="text/javascript">
+
+$(document).ready(function(){
+$('#upload').click(function(){
+var mygroup = $('#group').val();
+var image_url = $('#image_url').val();
+if(image_url == '')
+{
+ alert("Please enter image url");
+ return false;
+}
+else
+{
+ $('#upload').attr("disabled", "disabled");
+ $.ajax({
+  url:"upload.php",
+  method:"POST",
+  data:{image_url:image_url},
+  dataType:"JSON",
+  beforeSend:function(){
+   $('#upload').val("Processing...");
+  },
+  success:function(data)
+  {
+   $('#image_url').val('');
+   $('#upload').val('Upload');
+   $('#upload').attr("disabled", false);
+   $('#result').html(data.image);
+   alert(data.message);
+  }
+ })
+}
+});
+});
+
+  </script>
+     </div>
+
+
+   <!-- Trigger/Open The Modal -->
+    <button id="myBtntwo">Post Pictures</button>
+    <!-- The Modal -->
+    <div id="myModaltwo" class="modal">
+     <!-- Modal content -->
+
+     <div class="modal-content">
+       <span class="closetwo">&times;</span>
+       <p>Select a file to post</p>
+       <!-- <form class="w3-form-post" >
+         <input id="whatpic" type="file" name="see" required>
+        <input id="postUpload" type="submit" name="postUpload" value="Post">
+        <img id="blah" src="#" alt="#" width="200" height="200">
+       </form> -->
+     <div class="container" style="width:700px;">
+
+   <label>Select Image then click Done</label>
+
+   <input type="hidden" name="grouppic" id="grouppic" value=" <?php $currentgroup;?>">
+   <input type="file" name="file" id="file" />
+
+
+
+   <br />
+   <span id="uploaded_image"></span>
+   <form class="" action="post.php" method="post">
+   <input type="hidden" name="grouppic" id="grouppic" value=" <?php $currentgroup;?>">
+   <button class="closetwo" type="submit" name="button">Done</button>
+ </form>
+
+ </div>
+ </div>
+
+<script type="text/javascript">
+
+$(document).ready(function(){
+ $(document).on('change', '#file', function(){
+  var name = document.getElementById("file").files[0].name;
+  var form_data = new FormData();
+  form_data.append('grouppic',<?php echo $currentgroup;  ?>);
+  var ext = name.split('.').pop().toLowerCase();
+   // var grouppic = $('#grouppic').val();
+  if(jQuery.inArray(ext, ['gif','png','jpg','jpeg']) == -1)
+  {
+   alert("Invalid Image File");
+  }
+  var oFReader = new FileReader();
+  oFReader.readAsDataURL(document.getElementById("file").files[0]);
+  var f = document.getElementById("file").files[0];
+  var fsize = f.size||f.fileSize;
+  if(fsize > 2000000)
+  {
+   alert("Image File Size is very big");
+  }
+  else
+  {
+   form_data.append("file", document.getElementById('file').files[0]);
+   //form_data.append("grouppic", document.getElementById('grouppic'));
+   $.ajax({
+    url:"post.php",
+    method:"POST",
+    data:form_data,
+    contentType: false,
+    cache: false,
+    processData: false,
+    beforeSend:function(){
+     $('#uploaded_image').html("<label class='text-success'>Image Uploading...</label>");
+    },
+    success:function(data)
+    {
+     $('#uploaded_image').html(data);
+     alert("you won").html("<p>Bye</p>");
+    }
+   });
+  }
+ });
+});
+</script>
+
+    </div>
+    </div>
+   <span id='comment_message'></span>
    <br>
+
    <div class="table-responsive" id="pagination_data"></div>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
- <!-- <div id='display_comment'></div> -->
-</div>
+ </div>
 </div>
 </div>
     <?php
-} else {
+  } else {
     echo "No group exist";
-  }
-   ?>
+    }
+  ?>
   </body>
 </html>
-
 <script>
-function readURL(input) {
-  if (input.files && input.files[0]) {
-    var reader = new FileReader();
-    reader.onload = function(e) {
-      $('#blah').attr('src', e.target.result);
-    }
-    reader.readAsDataURL(input.files[0]);
-  }
-}
-$("#uploadFile").change(function() {
-  readURL(this);
-});
+
 $(document).ready(function(){
   $("#message").empty();
   $('#loading').show();
@@ -327,22 +467,39 @@ $(document).ready(function(){
 });
 // Get the modal
 var modal = document.getElementById('myModal');
+var Modaltwo = document.getElementById('myModaltwo');
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
+var btnTwo = document.getElementById("myBtntwo");
+
+
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
+var spanTwo = document.getElementsByClassName("closetwo")[0];
+
 // When the user clicks the button, open the modal
 btn.onclick = function() {
   modal.style.display = "block";
+}
+btnTwo.onclick = function() {
+  Modaltwo.style.display = "block";
 }
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
 }
+spanTwo.onclick = function() {
+  Modaltwo.style.display = "none";
+}
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
+  }
+}
+window.onclick = function(event) {
+  if (event.target == Modaltwo) {
+    Modaltwo.style.display = "none";
   }
 }
 </script>
--- post.php
+++ post.php
@@ -1,35 +1,29 @@
-<?php 
-if(isset($_POST["image_url"]))
+<?php
+session_start();
+$sessid = $_SESSION["userid"];
+$sessname = $_SESSION["username"];
+$group = $_POST['grouppic'];
+echo $group;
+// $data = isset($_REQUEST['grouppic'])?$_REQUEST['grouppic']:"";
+//     echo $data;
+$db = mysqli_connect("localhost", "root", "", "university");
+if($_FILES["file"]["name"] != '')
 {
- $message = '';
- $image = '';
- if(filter_var($_POST["image_url"], FILTER_VALIDATE_URL))
- {
-  $allowed_extension = array("jpg", "png", "jpeg", "gif");
-  $url_array = explode("/", $_POST["image_url"]);
-  $image_name = end($url_array);
-  $image_array = explode(".", $image_name);
-  $extension = end($image_array);
-  if(in_array($extension, $allowed_extension))
-  {
-   $image_data = file_get_contents($_POST["image_url"]);
-   $new_image_path = "upload/" . rand() . "." . $extension;
-   file_put_contents($new_image_path, $image_data);
-   $message = 'Image Uploaded';
-   $image = '<img src="'.$new_image_path.'" class="img-responsive img-thumbnail"  />';
-  }
-  else
-  {
-   $message = "Image not found";
-  }
- }
- else
- {
-  $message = 'Invalid Url';
- }
- $output = array(
-  'message' => $message,
-  'image'  => $image
- );
- echo json_encode($output);
+ $test = explode('.', $_FILES["file"]["name"]);
+ $ext = end($test);
+ $name = rand(100, 999) . '.' . $ext;
+ $location = './upload/' . $name;
+ move_uploaded_file($_FILES["file"]["tmp_name"], $location);
+ echo '<img src="'.$location.'" height="150" width="225" class="img-thumbnail" />';
+ $sql = "INSERT INTO images (image)
+          VALUES ('$location')";
+          mysqli_query($db, $sql);
+
+$how = "INSERT INTO tbl_comment(parent_comment_id, image, uid, comment_sender_name, grpid)
+        VALUES ('0','$location','$sessid','$sessname','$group')";
+        mysqli_query($db,$how);
+}
+if (isset($_POST['button'])) {
+  echo "well hello";
+
 }
